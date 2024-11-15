94c91f4b62ea5c948f9a50440600fda42ac88052
Harshil Agrawal <harshil@cloudflare.com>
2024-11-16 00:08:16 +0530

    use KV default

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
 import { routePartykitRequest, Server, type Connection, type WSMessage } from 'partyserver';
-// Define your Server
+
 export class VotingServer extends Server {
 	// store the data in memory
-	choice: Record<string, number> = {};
+	// choice: Record<string, number> = {};
 	// use the KV storage for persistent storage
 	kv = this.ctx.storage;
 	async onConnect(connection: Connection) {
 		console.log('Connected', connection.id, 'to server', this.name);
-		// const votes = (await this.kv.get('fav-avengers')) || {};
-		// connection.send(JSON.stringify({ type: 'result', results: votes }));
+
+		// Send votes to the newly connected users
+		const votes = (await this.kv.get('fav-avengers')) || {};
+		connection.send(JSON.stringify({ type: 'result', results: votes }));
 		// connection.send(JSON.stringify({ type: 'result', results: this.choice }));
 	}
 
 	async onMessage(connection: Connection, message: WSMessage) {
 		const m: { type: string; choice: string } = JSON.parse(message.toString());
 		if (m.type === 'vote') {
-			// const votes: Record<string, number> = (await this.kv.get('fav-avengers')) || {};
-			// votes[m.choice] = (votes[m.choice] || 0) + 1;
-			// await this.kv.put('fav-avengers', votes);
-			// this.broadcast(JSON.stringify({ type: 'result', results: votes }));
+			const votes: Record<string, number> = (await this.kv.get('fav-avengers')) || {};
+			votes[m.choice] = (votes[m.choice] || 0) + 1;
+			await this.kv.put('fav-avengers', votes);
+			this.broadcast(JSON.stringify({ type: 'result', results: votes }));
 
 			// this.choice[m.choice] = (this.choice[m.choice] || 0) + 1;
-			this.broadcast(JSON.stringify({ type: 'result', results: this.choice }));
+			// this.broadcast(JSON.stringify({ type: 'result', results: this.choice }));
 		}
 	}
 }
